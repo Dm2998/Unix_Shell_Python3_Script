0f7c7130f388a023fe9ceeb8d071b3ebbe5ff838
Diego Alexander <114578666+Dm2998@users.noreply.github.com>
2022-12-30 12:15:29 +0000

    Update Shell_Python3_Scripting.py

--- Shell_Python3_Scripting.py
+++ Shell_Python3_Scripting.py
@@ -9,37 +9,39 @@
 # c) System date and time
 # d) Show the current username
 
-#!#/usr/bin/env python3
+!#/usr/bin/env python3             # shebang line to run python3 script in linux environment.
 
-import subprocess
-import os
-import datetime
+import subprocess                    # for calling shell commands
+import os                             # for getting current username
+import datetime                       # for getting current date and time
 directory_name = ['testdir1', 'testdir2', 'testdir3', 'testdir4', 'testdir5']
 
 
-def create_directory(directory_name):
-    for name in directory_name:
-        subprocess.call(['mkdir', name.lower()])
-        print('Directory created: ', name.lower())
+def create_directory(directory_name):           # create directories
+    for name in directory_name:                 # loop through directory names
+        subprocess.call(['mkdir', name.lower()])     # create directories in lower case mkdir = make directory 
+        print('Directory created: ', name.lower())       # print directory name in lower case
     return directory_name
 
-def show_disk_usage():          
-    subprocess.call(['df', '-h'])
+def show_disk_usage():           # show disk usage in human readable format
+    subprocess.call(['df', '-h'])          # df = disk free, -h = human readable format
     
-def list_directory_contents():
-    subprocess.call(['ls', '-l'])
+def list_directory_contents():              # list directory contents
+    subprocess.call(['ls', '-l'])          # ls = list, -l = long format
     print('Directory contents: ', directory_name)
    
      
-def directory_date_time():
+def directory_date_time():                    # show current date and time
     subprocess.call(['ls', '-l'])
     print(datetime.datetime.now())
     
-def show_current_username():
-    subprocess.call(['whoami'])
-    print(os.getlogin())
+def show_current_username():                   # show current username
+    subprocess.call(['whoami'])                # call whoami command to show current username. linux command
+    print(os.getlogin())                       # getlogin() method to show current username python method
 
-if __name__ == '__main__':
+   
+
+if __name__ == '__main__':                   # main function to call all other functions
     directory_name = create_directory(directory_name)
     show_disk_usage()
     list_directory_contents()
